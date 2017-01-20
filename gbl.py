4d3f120d72959892efbf704d598fbf847d099153
Ryan <rab83@duke.edu>
2017-01-20 13:02:48 -0500

    gatherbasiclevel commit with comments added

--- gbl.py
+++ gbl.py
@@ -9,8 +9,10 @@ output_dir = ""
 subject= ""
 month = ""
 
+#walk_tree traverses start_dir alterting format and output_dir
 def walk_tree():
     for root, dirs, files in os.walk(start_dir):
+        #Check if audio or video analysis before manipulation
         if os.path.split(root)[1] == "Audio_Analysis":
             for file in files:
                 if month:
@@ -21,6 +23,7 @@ def walk_tree():
                 else:
                     if correct_audio_bl_name(file):
                         copy(os.path.join(root, file), output_dir)
+        #Check if audio or video analysis before manipulation
         if os.path.split(root)[1] == "Video_Analysis":
             for file in files:
                 if month:
@@ -33,12 +36,14 @@ def walk_tree():
                         copy(os.path.join(root, file), output_dir)
 
 
+#Helper function used in walk_tree -- boolean (check for .csv; audio; ready; ".")
 def correct_audio_bl_name(name):
     if name.endswith(".csv") and ("audio" in name)\
         and ("ready" not in name) and (not name.startswith(".")):
             return True
     return False
 
+#Helper function used in walk_tree -- boolean (check for .csv; video; ready; ".")
 def correct_video_bl_name(name):
     if name.endswith(".csv") and ("video" in name)\
         and ("ready" not in name) and (not name.startswith(".")):
@@ -47,9 +52,11 @@ def correct_video_bl_name(name):
 
 if __name__ == "__main__":
 
+    #Initialize and assign values to start_dir; output_dir
     start_dir = sys.argv[1]
     output_dir = sys.argv[2]
 
+    #Initialize and assign values to "subject" and "month" 
     subject = sys.argv[3]
     month = sys.argv[4]
 
